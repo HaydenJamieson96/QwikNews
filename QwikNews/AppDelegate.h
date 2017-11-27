4c8ed37ce81bd94cb8bf25a0817ddabfd7cdaf93
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-11-27 11:22:34 +0000

    Initial commit

---
+++ QwikNews/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  QwikNews
+//
+//  Created by TheAppExperts on 11/27/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ QwikNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QwikNews
+//
+//  Created by TheAppExperts on 11/27/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
